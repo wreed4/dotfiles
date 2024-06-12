4fe53bf65b65cf019738b47b4024270658e030e8
William Reed <willie.b.reed@gmail.com>
2024-06-12 00:16:20 -0400

    added alacritty and zellij configs

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+echo ". $HOME/.dotfiles/path_setup.sh" >> $HOME/.profile
+
 files=(bash_profile bashrc inputrc tmux.conf xonshrc gitconfig vim taskrc)
 
 for f in ${files[@]}; do
     echo ~/.$f "-->" $(pwd)/$f
     [[ -f ~/.$f ]] && mv ~/.$f ~/$f.old
-    ln -s $(pwd)/$f ~/.$f
+    ln -s --backup=numbered $(pwd)/$f ~/.$f
 done
 
 ./vim/make_links.sh
 
 echo ~/.config/helix/conf.toml "-->" $(pwd)/helixconf.toml
-ln -s $(pwd)/helixconf.toml ~/.config/helix/config.toml
+ln -s --backup=numbered $(pwd)/helixconf.toml ~/.config/helix/config.toml
+
+[[ -d ~/.config/zellij ]] || mkdir $HOME/.config/zellij
+ln -s --backup=numbered $(pwd)/zellij_config.kdl ~/.config/zellij/config.kdl
+
+[[ -d ~/.config/alacritty ]] || mkdir $HOME/.config/alacritty
+ln -s --backup=numbered $(pwd)/alacritty.toml ~/.config/alacritty/alacritty.toml
+
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -i ~/.bashrc ~/.tmux.conf ~/.xonshrc ~/.gitconfig ~/.vim ~/.config/nvim ~/.taskrc
+rm -i ~/.bashrc ~/.tmux.conf ~/.xonshrc ~/.gitconfig ~/.vim ~/.config/nvim ~/.taskrc ~/.config/helix/config.toml ~/.config/zellij/config.kdl ~/.config/alacritty/alacritty.toml
