fdd08b20798bcb0088a44cac717095a5ae9c942a
William Reed <willie.b.reed@gmail.com>
2025-02-27 01:13:54 -0500

    added go path

--- path_setup.sh
+++ path_setup.sh
@@ -6,6 +6,9 @@ fi
 if [ -d "$HOME/go/bin" ] ; then
     export PATH="$HOME/go/bin:$PATH"
 fi
+if [ -d "/usr/local/go/bin" ] ; then
+    export PATH="/usr/local/go/bin:$PATH"
+fi
 if [ -d "$HOME/.krew/bin" ] ; then
     export PATH="$HOME/.krew/bin:$PATH"
 fi
